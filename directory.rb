77280bb0491cc424ffb59c6abcf96f44f5fd00d2
Antonio <ciniglioantonio@gmail.com>
2018-02-12 18:05:33 +0000

    Add exercises 8.1, 8.2, 8.3, 8.4

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-# let's put all students into an array
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -22,11 +21,14 @@ def print_header
 puts "The students of Villains Academy"
 puts "-------------"
 end
-
-#def print(students)
-#  puts "Student names beginning with: (Please enter a letter)"
-#  letter = gets.chomp
 def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+#8.4
+def print_with_loop(students)
   x = 0
   while x < students.count
     puts "#{students[x][:name]} (#{students[x][:cohort]})"
@@ -34,8 +36,46 @@ def print(students)
   end
 end
 
+#8.3
+def print_by_name_length(students)
+  puts "Names with maximum length of (please enter a number):"
+  max_length = gets.chomp
+  matching_students = 0
+  students.each do |student|
+    if student[:name].length <= max_length.to_i
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+      matching_students += 1
+    else
+    end
+  end
+  puts
+  puts "There are #{matching_students} students whose maximum name's length is #{max_length} characters."
+  puts
+end
 
+#8.2
+def print_specific_first_letter(students)
+  puts "Student names beginning with: (Please enter a letter)"
+  letter = gets.chomp
+  matching_students = 0
+  students.each do |student|
+    if student[:name].start_with?(letter.upcase, letter.downcase)
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+      matching_students += 1
+    else
+    end
+  end
+  puts
+  puts "#{matching_students} students' name begins with #{letter}"
+  puts
+end
 
+#8.1
+def print_number_before(students)
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
 
 
 def print_footer(students)
