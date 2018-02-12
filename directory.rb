c2f4fac9582b7cf057722395a8e5cfe7c86458f9
Antonio <ciniglioantonio@gmail.com>
2018-02-12 19:39:51 +0000

    Format a bit, ex8.6; deal with cohort input, ex8.7

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+#8.6
+@width = 50
 #8.5
 def input_students
   puts "Please enter the name of the student"
@@ -13,7 +15,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
   # add the student hash to the array
-    students << {name: name, cohort: :november, hobbies: more_info[:hobbies],
+    students << {name: name, cohort: more_info[:cohort], hobbies: more_info[:hobbies],
                  country: more_info[:country], height: more_info[:height]}
     puts "Now we have #{students.count} students"
     # get another name from the user
@@ -27,24 +29,35 @@ def input_students
   students
 end
 
-def info
+def info()
+  months = ["january", "february", "march", "april", "may", "june", "july",
+                    "august", "september", "october", "november", "december"]
+  puts "Enter your cohort"
+  cohort = gets.chomp
+  while !months.include? cohort.downcase || cohort != "" do
+    puts "Please enter a valid month"
+    cohort = gets.chomp
+  end
+  if cohort == ""
+    cohort = :february
+end
   puts "Enter the student's hobbies separated by a comma"
   hobbies = gets.chomp
   puts "Enter the student's country of origin"
   country = gets.chomp.capitalize
   puts "Enter the student's height"
   height = gets.chomp.to_i
-  return {hobbies: hobbies, country: country, height: height}
+  return {cohort: cohort.downcase.to_sym, hobbies: hobbies, country: country, height: height}
 end
 
 def print_header
-puts "The students of Villains Academy"
-puts "-------------"
+puts "The students of Villains Academy".center(@width)
+puts "-------------".center(@width)
 end
 
 def print(students)
   students.each do |student|
-    puts "Name: #{student[:name]} | Cohort: #{student[:cohort]} | Hobbies: #{student[:hobbies]} | Country of origin: #{student[:country]} | Height: #{student[:height]}"
+    puts "Name: #{student[:name]} | Cohort: #{student[:cohort]} | Hobbies: #{student[:hobbies]} | Country of origin: #{student[:country]} | Height: #{student[:height]}".center(@width)
   end
 end
 
@@ -52,55 +65,55 @@ end
 def print_with_loop(students)
   x = 0
   while x < students.count
-    puts "#{students[x][:name]} (#{students[x][:cohort]})"
+    puts "#{students[x][:name]} (#{students[x][:cohort]})".center(@width)
     x += 1
   end
 end
 
 #8.3
 def print_by_name_length(students)
-  puts "Names with maximum length of (please enter a number):"
+  puts "Names with maximum length of (please enter a number):".center(@width)
   max_length = gets.chomp
   matching_students = 0
   students.each do |student|
     if student[:name].length <= max_length.to_i
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{student[:name]} (#{student[:cohort]} cohort)".center(@width)
       matching_students += 1
     else
     end
   end
   puts
-  puts "There are #{matching_students} students whose maximum name's length is #{max_length} characters."
+  puts "There are #{matching_students} students whose maximum name's length is #{max_length} characters.".center(@width)
   puts
 end
 
 #8.2
 def print_specific_first_letter(students)
-  puts "Student names beginning with: (Please enter a letter)"
+  puts "Student names beginning with: (Please enter a letter)".center(@width)
   letter = gets.chomp
   matching_students = 0
   students.each do |student|
     if student[:name].start_with?(letter.upcase, letter.downcase)
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{student[:name]} (#{student[:cohort]} cohort)".center(@width)
       matching_students += 1
     else
     end
   end
   puts
-  puts "#{matching_students} students' name begins with #{letter}"
+  puts "#{matching_students} students' name begins with #{letter}".center(@width)
   puts
 end
 
 #8.1
 def print_number_before(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(@width)
   end
 end
 
 
 def print_footer(students)
-puts "Overall, we have #{students.count} great students"
+puts "Overall, we have #{students.count} great students".center(@width)
 end
 # nothing happens until we call the methods
 students = input_students
