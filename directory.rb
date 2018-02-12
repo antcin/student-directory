61683485e95b8c213f0ac89cd1243c77982c184a
Antonio <ciniglioantonio@gmail.com>
2018-02-12 16:21:00 +0000

    Add student number, ex 8.1

--- directory.rb
+++ directory.rb
@@ -1,18 +1,36 @@
 # let's put all students into an array
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
+  # create an empty array
   students = []
   # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
+  # while the name is not empty, repeat this code
+  puts "Please enter the cohort of the student"
+  cohort = gets.chomp
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
+    # add the student hash to the array
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
+    puts "Please enter the cohort of the student"
+    cohort = gets.chomp
   end
   # return the array of students
   students
@@ -23,8 +41,8 @@ puts "The students of Villains Academy"
 puts "-------------"
 end
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(students)
