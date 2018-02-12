2a4bc5aef68df81d5d1d82a03e4b6030f48cf803
Antonio <ciniglioantonio@gmail.com>
2018-02-12 16:35:40 +0000

    Change print(students) method to print user by first letter, ex 8.2

--- directory.rb
+++ directory.rb
@@ -1,36 +1,18 @@
 # let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
+  # create an empty array
   students = []
   # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
-  puts "Please enter the cohort of the student"
-  cohort = gets.chomp
+  # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: cohort}
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
-    puts "Please enter the cohort of the student"
-    cohort = gets.chomp
   end
   # return the array of students
   students
@@ -40,10 +22,21 @@ def print_header
 puts "The students of Villains Academy"
 puts "-------------"
 end
+
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  puts "Student names beginning with: (Please enter a letter)"
+  letter = gets.chomp
+  matching_students = 0
+  students.each do |student|
+    if student[:name].start_with?(letter.upcase, letter.downcase)
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+      matching_students += 1
+    else
+    end
   end
+  puts
+  puts "#{matching_students} students' name begins with #{letter}"
+  puts
 end
 def print_footer(students)
 puts "Overall, we have #{students.count} great students"
